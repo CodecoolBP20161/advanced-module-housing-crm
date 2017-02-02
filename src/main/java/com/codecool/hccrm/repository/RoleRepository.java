44b1e52d878579e4065cb73e25ba404ed8cbdc96
dsztanko <szt.dori@gmail.com>
2017-02-03 00:29:37 +0100

    adding RoleRepository

---
+++ src/main/java/com/codecool/hccrm/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.hccrm.repository;
+
+import com.codecool.hccrm.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
