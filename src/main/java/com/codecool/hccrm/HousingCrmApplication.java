ff07e24490eef4cadec7f79c72a2193d1be52c09
dsztanko <szt.dori@gmail.com>
2017-02-02 23:32:16 +0100

    basic structure and simply working index page rendering implemented, spring banner from console removed

---
+++ src/main/java/com/codecool/hccrm/HousingCrmApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.hccrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingCrmApplication.class, args);
+	}
+}
