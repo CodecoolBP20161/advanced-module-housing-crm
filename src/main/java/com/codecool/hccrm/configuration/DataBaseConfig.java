731d857afaccad1b183a4c5e3f1c5d06eaf951b7
balag3 <carbalage@gmail.com>
2017-02-04 22:19:01 +0100

    Adds javadoc comment

--- src/main/java/com/codecool/hccrm/configuration/DataBaseConfig.java
+++ src/main/java/com/codecool/hccrm/configuration/DataBaseConfig.java
@@ -11,8 +11,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DataBaseConfig {
 
+    /**
+     * get hibernate'a sessionFactory ready for injection, so we can create the current session object
+     * to run queries
+     * @param hemf giving access to the underlying SessionFactory from an EntityManagerFactory
+     * @return sessionFactory which provides us the current session object
+     * representing an internal state including all of the metadata about Object/Relational Mapping.
+     */
     @Bean
-    public SessionFactory sessionFactory(HibernateEntityManagerFactory hemf){
+    public SessionFactory sessionFactory(HibernateEntityManagerFactory hemf) {
         return hemf.getSessionFactory();
     }
 }
