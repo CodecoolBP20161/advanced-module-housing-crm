b56cff4bc09c2617c734e795bcc213155b34b61a
balag3 <carbalage@gmail.com>
2017-02-04 17:39:25 +0100

    Adds CondominiumQueryRepository and its implementation to run Condominium related queries

---
+++ src/main/java/com/codecool/hccrm/repository/CondominiumQueryRepositoryImpl.java
@@ -0,0 +1,28 @@
+package com.codecool.hccrm.repository;
+
+import com.codecool.hccrm.model.Condominium;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by balag3 on 2017.02.04..
+ */
+@Repository
+public class CondominiumQueryRepositoryImpl implements CondominiumQueryRepository {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    private Session session() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    @Override
+    public List<Condominium> selectAll() {
+        return session().createQuery("from Condominium").list();
+    }
+}
