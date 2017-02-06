2618c8495ce33f403419656e04b54606d6274862
dsztanko <szt.dori@gmail.com>
2017-02-06 10:47:05 +0100

    sample using of logging

--- src/main/java/com/codecool/hccrm/controller/PageController.java
+++ src/main/java/com/codecool/hccrm/controller/PageController.java
@@ -1,6 +1,9 @@
 package com.codecool.hccrm.controller;
 
+import com.codecool.hccrm.logging.LogFormatter;
 import com.codecool.hccrm.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +11,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PageController {
+    private Logger logger = LoggerFactory.getLogger(PageController.class);
 
     @Autowired
     TestService service;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() throws Exception {
+        logger.info(LogFormatter.FORMAT.getFormatter() + "Index page is called.");
+
         service.testClient();
         return "index";
     }
