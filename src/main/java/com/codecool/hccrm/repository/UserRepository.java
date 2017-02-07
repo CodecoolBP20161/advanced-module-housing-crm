c77e9ceb54841e54309ba587b252650ff4e7e6fc
balag3 <carbalage@gmail.com>
2017-02-07 11:05:45 +0100

    Adds @RepositoryRestResource to all repositories to push data in Json

--- src/main/java/com/codecool/hccrm/repository/UserRepository.java
+++ src/main/java/com/codecool/hccrm/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.codecool.hccrm.repository;
 
 import com.codecool.hccrm.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends JpaRepository<User, Long> {
 }
