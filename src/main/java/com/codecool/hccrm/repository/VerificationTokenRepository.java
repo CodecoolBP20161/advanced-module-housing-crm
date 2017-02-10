12f3f550a7405f8a44cabb984e93d44112317f09
balag3 <carbalage@gmail.com>
2017-02-10 10:17:31 +0100

    adds missing repository annotation

--- src/main/java/com/codecool/hccrm/repository/VerificationTokenRepository.java
+++ src/main/java/com/codecool/hccrm/repository/VerificationTokenRepository.java
@@ -3,10 +3,12 @@ package com.codecool.hccrm.repository;
 import com.codecool.hccrm.model.User;
 import com.codecool.hccrm.model.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by prezi on 2017. 02. 06..
  */
+@Repository
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
     VerificationToken findByToken(String token);
 
