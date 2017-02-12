1355c5532332b63c5f589b99e690ce3df3be4fdf
turbek <turbek93@gmail.com>
2017-02-12 21:19:24 +0100

    added additional filters for table

--- src/main/resources/static/js/table.js
+++ src/main/resources/static/js/table.js
@@ -1,12 +1,39 @@
-$('#company_list').DataTable( {
-    ajax: {
-        url: 'admin/company',
-        dataSrc: '_embedded.company'
-    },
-    columns: [
-        { data: 'companyName' },
-        { data: 'taxNumber' },
-        { data: 'premise' },
-        { data: 'regDate' }
-    ]
-} );
+$(document).ready(function() {
+    // Setup - add a text input to each footer cell
+    $('#company_list tfoot th').each(function () {
+        var title = $(this).text();
+        $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+    });
+
+    // Setup datatable
+    $('#company_list').DataTable({
+        ajax: {
+            stateSave: true,
+            url: 'admin/company',
+            dataSrc: '_embedded.company'
+        },
+        columns: [
+            {data: 'companyName'},
+            {data: 'taxNumber'},
+            {data: 'premise'},
+            {data: 'regDate'}
+        ]
+    });
+
+    // // TEST
+    // localStorage.setItem( 'mykey', JSON.stringify( "asdasd" ) );
+
+    // Apply the search
+    table.columns().every(function () {
+        var that = this;
+
+        $('input', this.footer()).on('keyup change', function () {
+            if (that.search() !== this.value) {
+                that
+                    .search(this.value)
+                    .draw();
+            }
+        });
+    });
+
+});
