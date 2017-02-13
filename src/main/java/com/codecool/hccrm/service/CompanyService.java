09bab32f53fc11bb1be376a621e2f354d1996864
dsztanko <szt.dori@gmail.com>
2017-02-13 03:20:01 +0100

    find() method added to Company repository and service layer

--- src/main/java/com/codecool/hccrm/service/CompanyService.java
+++ src/main/java/com/codecool/hccrm/service/CompanyService.java
@@ -1,5 +1,7 @@
 package com.codecool.hccrm.service;
 
+import com.codecool.hccrm.dto.CompanyDTO;
+import com.codecool.hccrm.error.CompanyAlreadyExistsException;
 import com.codecool.hccrm.model.Company;
 import com.codecool.hccrm.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,5 +35,23 @@ public class CompanyService {
 
     public List<Company> findAll() {
         return companyRepository.findAll();
+    }
+
+    public Company findByPremiseAndTaxNumber(String premise, String taxNumber) {
+        return companyRepository.findByPremiseAndTaxNumber(premise, taxNumber);
+    }
+
+    @Transactional
+    public Company createNewCompany(CompanyDTO dto) throws CompanyAlreadyExistsException {
+        if (alreadyExists(dto.getPremise(), dto.getTaxNumber())) {
+            throw new CompanyAlreadyExistsException("Already have a company with tax number " + dto.getTaxNumber() + " in " + dto.getPremise() + " registered.");
+        }
+        Company company  = new Company(dto.getCompanyName(), dto.getTaxNumber(), dto.getPremise());
+        return companyRepository.save(company);
+    }
+
+    private boolean alreadyExists(String premise, String taxNumber) {
+        Company company = findByPremiseAndTaxNumber(premise, taxNumber);
+        return company != null;
     }
 }
