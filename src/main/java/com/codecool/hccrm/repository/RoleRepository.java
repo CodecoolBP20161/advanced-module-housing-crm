e3685357d82670ca8b2916fca5ef47dbed5caed8
dsztanko <szt.dori@gmail.com>
2017-02-13 03:18:19 +0100

    adding find() method to RoleRepository to retrieve Roles from db

--- src/main/java/com/codecool/hccrm/repository/RoleRepository.java
+++ src/main/java/com/codecool/hccrm/repository/RoleRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RepositoryRestResource(collectionResourceRel = "role", path = "role")
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
 }
