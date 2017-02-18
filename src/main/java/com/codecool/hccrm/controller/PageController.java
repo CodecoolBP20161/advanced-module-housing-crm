14c3843481d2bff55653a62d71f424639d0fd3cc
dsztanko <szt.dori@gmail.com>
2017-02-18 21:09:10 +0100

    optimize imports

--- src/main/java/com/codecool/hccrm/controller/PageController.java
+++ src/main/java/com/codecool/hccrm/controller/PageController.java
@@ -1,11 +1,12 @@
 package com.codecool.hccrm.controller;
 
-import com.codecool.hccrm.logging.LogFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import static com.codecool.hccrm.logging.LogFormatter.FORMAT;
 
 @Controller
 public class PageController {
@@ -13,7 +14,7 @@ public class PageController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() throws Exception {
-        logger.info(LogFormatter.FORMAT.getFormatter() + "Index page is called.");
+        logger.info(FORMAT.getFormatter() + "Index page is called.");
 
         return "index";
     }
