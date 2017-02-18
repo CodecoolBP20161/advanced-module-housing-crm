c230ef8d2dbe9465cf3f90ab69fe07d28625ce9c
dsztanko <szt.dori@gmail.com>
2017-02-18 21:05:59 +0100

    deletes unused codebase from TestService and PageController

--- src/main/java/com/codecool/hccrm/controller/PageController.java
+++ src/main/java/com/codecool/hccrm/controller/PageController.java
@@ -1,10 +1,8 @@
 package com.codecool.hccrm.controller;
 
 import com.codecool.hccrm.logging.LogFormatter;
-import com.codecool.hccrm.service.TestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,14 +11,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class PageController {
     private Logger logger = LoggerFactory.getLogger(PageController.class);
 
-    @Autowired
-    TestService service;
-
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() throws Exception {
         logger.info(LogFormatter.FORMAT.getFormatter() + "Index page is called.");
 
-        service.testClient();
         return "index";
     }
 
