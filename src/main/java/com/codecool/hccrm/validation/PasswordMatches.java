2e976badb793d0ce774e90213633e339f8a4483a
dsztanko <szt.dori@gmail.com>
2017-02-19 00:30:22 +0100

    PasswordMatcher imlemented

--- src/main/java/com/codecool/hccrm/validation/PasswordMatches.java
+++ src/main/java/com/codecool/hccrm/validation/PasswordMatches.java
@@ -1,30 +1,23 @@
 package com.codecool.hccrm.validation;
 
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import com.codecool.hccrm.validation.implementations.PasswordMatchesValidator;
 
+import javax.validation.Constraint;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import javax.validation.Constraint;
-import javax.validation.Payload;
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Created by prezi on 2017. 02. 07..
- * <p>
- * This is what basically creates the @PasswordChecked decorator
  */
 @Target({TYPE, ANNOTATION_TYPE})
 @Retention(RUNTIME)
-@Constraint(validatedBy = PasswordCheckedValidator.class)
+@Constraint(validatedBy = PasswordMatchesValidator.class)
 @Documented
-public @interface PasswordChecked {
-
-    String message() default "Passwords don't match";
-
-    Class<?>[] groups() default {};
-
-    Class<? extends Payload>[] payload() default {};
+public @interface PasswordMatches {
+    String message() default "Passwords don't match.";
 }
--- src/main/java/com/codecool/hccrm/validation/implementations/PasswordMatchesValidator.java
+++ src/main/java/com/codecool/hccrm/validation/implementations/PasswordMatchesValidator.java
@@ -1,23 +1,24 @@
-//package com.codecool.hccrm.validation;
-//
-///**
-// * Created by prezi on 2017. 02. 07..
-// */
-//
-//import javax.validation.ConstraintValidator;
-//import javax.validation.ConstraintValidatorContext;
-//
-//public class PasswordCheckedValidator implements ConstraintValidator<PasswordChecked, Object> {
-//
-//    @Override
-//    public void initialize(final PasswordChecked constraintAnnotation) {
-//        //
-//    }
-//
-//    @Override
-//    public boolean isValid(final Object obj, final ConstraintValidatorContext context) {
-//        final  user = (UserDTO) obj;
-//        return user.getPassword().equals(user.getPasswordCheck());
-//    }
-//
-//}
+package com.codecool.hccrm.validation.implementations;
+
+/**
+ * Created by prezi on 2017. 02. 07..
+ */
+
+import com.codecool.hccrm.dto.SignUpDTO;
+import com.codecool.hccrm.validation.PasswordMatches;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+
+    @Override
+    public void initialize(PasswordMatches constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(Object obj, ConstraintValidatorContext context){
+        SignUpDTO dto = (SignUpDTO) obj;
+        return dto.getPassword().equals(dto.getMatchingPassword());
+    }
+}
