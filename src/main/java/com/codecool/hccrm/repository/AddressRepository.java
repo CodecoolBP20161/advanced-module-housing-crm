6b04df6b13727e8a4fdb151dcbf025f59eb6686c
dsztanko <szt.dori@gmail.com>
2017-02-19 13:44:20 +0100

    adding RestResource

---
+++ src/main/java/com/codecool/hccrm/repository/AddressRepository.java
@@ -0,0 +1,15 @@
+package com.codecool.hccrm.repository;
+
+import com.codecool.hccrm.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by balag3 on 2017.02.17..
+ * Last edited by dorasztanko on 2017.02.19..
+ */
+@Repository
+@RepositoryRestResource(collectionResourceRel = "address", path = "address")
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
