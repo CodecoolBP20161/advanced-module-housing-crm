4f456a556425ae8bb156c840789bd2ee9fa9c7aa
balag3 <carbalage@gmail.com>
2017-02-20 10:37:48 +0100

    Adds NameValidator annotation.

--- src/main/java/com/codecool/hccrm/validation/ValidPersonName.java
+++ src/main/java/com/codecool/hccrm/validation/ValidPersonName.java
@@ -1,6 +1,6 @@
 package com.codecool.hccrm.validation;
 
-import com.codecool.hccrm.validation.constraint.PhoneConstraintValidator;
+import com.codecool.hccrm.validation.constraint.PersonNameValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
@@ -8,23 +8,21 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Created by dorasztanko on 2017.02.19..
+ * Created by balag3 on 2017.02.20..
  */
-@Target({METHOD, FIELD})
+@Target({TYPE, ANNOTATION_TYPE, FIELD})
 @Retention(RUNTIME)
-@Constraint(validatedBy = PhoneConstraintValidator.class)
+@Constraint(validatedBy = PersonNameValidator.class)
 @Documented
-public @interface Phone {
-    String message() default "Not a valid phone number.";
+public @interface ValidPersonName {
+
+    String message() default "Invalid name.";
 
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
-
 }
-
