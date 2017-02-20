6835b9cab34616e99fdc7fc05c1ebe4453730bb0
balag3 <carbalage@gmail.com>
2017-02-20 11:40:11 +0100

    security redirecting handled

--- src/main/java/com/codecool/hccrm/configuration/AdminConfiguration.java
+++ src/main/java/com/codecool/hccrm/configuration/AdminConfiguration.java
@@ -1,22 +1,42 @@
 package com.codecool.hccrm.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+
+import static com.codecool.hccrm.model.RoleEnum.ROLE_ADMIN;
+import static com.codecool.hccrm.model.RoleEnum.ROLE_CEO;
 
 /**
  * Created by balag3 on 2017.02.09..
  */
 @Configuration
 public class AdminConfiguration extends WebSecurityConfigurerAdapter {
+    private static final Logger logger = LoggerFactory.getLogger(AdminConfiguration.class);
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -29,15 +49,17 @@ public class AdminConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/regitrationConfirm", "/signup").permitAll()
-                .antMatchers("/","/user/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/", "/user/**").hasAnyRole("MANAGER", "ADMIN", "CEO")
                 .antMatchers("/admin/**").hasRole("ADMIN")
-            .anyRequest().authenticated()
+                .anyRequest().authenticated()
                 .and()
-            .formLogin()
+                .formLogin()
                 .loginPage("/login")
+                .successHandler(authenticationSuccessHandler())
+                .failureHandler(authenticationFailureHandler())
                 .permitAll()
                 .and()
-            .logout()
+                .logout()
                 .logoutSuccessUrl("/login?logout")
                 .permitAll();
     }
@@ -47,4 +69,68 @@ public class AdminConfiguration extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
+
+    @Bean
+    AuthenticationSuccessHandler authenticationSuccessHandler() {
+
+        return new SimpleUrlAuthenticationSuccessHandler() {
+
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request,
+                                                HttpServletResponse response,
+                                                Authentication authentication) throws IOException, ServletException {
+
+                String targetUrl = determineTargetUrl(request, response, authentication);
+
+                if (response.isCommitted()) {
+                    logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
+                    return;
+                }
+
+                getRedirectStrategy().sendRedirect(request, response, targetUrl);
+            }
+
+            protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                boolean isUser = false;
+                boolean isAdmin = false;
+                Collection<? extends GrantedAuthority> authorities
+                        = authentication.getAuthorities();
+                for (GrantedAuthority grantedAuthority : authorities) {
+                    if (grantedAuthority.getAuthority().equals(ROLE_CEO.getRole())) {
+                        isUser = true;
+                        break;
+                    } else if (grantedAuthority.getAuthority().equals(ROLE_ADMIN.getRole())) {
+                        isAdmin = true;
+                        break;
+                    }
+                }
+
+                if (isUser) {
+                    return "/user/dashboard";
+                } else if (isAdmin) {
+                    return "/admin";
+                } else {
+                    throw new IllegalStateException();
+                }
+            }
+        };
+    }
+
+    /**
+     * Creates a custom AuthenticationFailureHandler
+     * It returns custom errors with {@link HttpServletResponse#SC_UNAUTHORIZED} (401) HTTP Status.
+     *
+     * @return AuthenticationFailureHandler
+     */
+    @Bean
+    AuthenticationFailureHandler authenticationFailureHandler() {
+        return (request, response, exception) -> {
+            if (exception instanceof DisabledException) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "User account suspended");
+            } else {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication failed");
+            }
+        };
+    }
+
 }
