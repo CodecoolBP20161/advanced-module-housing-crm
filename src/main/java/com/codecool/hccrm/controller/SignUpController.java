e04960f5f7c7f8b27cda2fe7aa31d6b00173b6f0
robotkutya <mihalyfuredi@gmail.com>
2017-03-03 10:44:33 +0100

    test deploy

--- src/main/java/com/codecool/hccrm/controller/SignUpController.java
+++ src/main/java/com/codecool/hccrm/controller/SignUpController.java
@@ -91,7 +91,7 @@ public class SignUpController {
         if (verificationToken == null) {
             String message = messages.getMessage("auth.message.invalidToken", null, locale);
             model.addAttribute("message", message);
-            return "verification_bad";
+            return "login_verified";
         }
 
         User user = verificationToken.getUser();
@@ -99,7 +99,7 @@ public class SignUpController {
         if ((verificationToken.getExpirationDate().getTime() - cal.getTime().getTime()) <= 0) {
             String messageValue = messages.getMessage("auth.message.expired", null, locale);
             model.addAttribute("message", messageValue);
-            return "verification_bad";
+            return "login_verified";
         }
 
         user.setVerified(true);
