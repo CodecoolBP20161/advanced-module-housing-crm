6203c7d0ef92f7331ba95c09fa5368ba08a41923
turbek <turbek93@gmail.com>
2017-03-08 12:31:40 +0100

    changed jquery to redirect to login after click

--- src/main/resources/static/js/signup/signup.js
+++ src/main/resources/static/js/signup/signup.js
@@ -1,30 +1,28 @@
 $(document).ready(function(){
 
-    $("#userinfo").addClass("tab-selected");
-    $("#signup_step2").hide();
-    $("#signup_step3").hide();
+    $(".userinfo").addClass("tab-selected");
+    $(".signup_step2").hide();
+    $(".signup_step3").hide();
 
-    $("#next1").click(function(){
-        $("#signup_step1").hide();
-        $("#signup_step2").show();
-        $("#userinfo").removeClass("tab-selected");
-        $("#companyinfo").addClass("tab-selected");
+    $(".next1").click(function(){
+        $(".signup_step1").hide();
+        $(".signup_step2").show();
+        $(".userinfo").removeClass("tab-selected");
+        $(".companyinfo").addClass("tab-selected");
         $('html, body').animate({scrollTop: '0px'}, 300);
     });
 
-    $("#back2").click(function(){
-        $("#signup_step1").show();
-        $("#signup_step2").hide();
-        $("#companyinfo").removeClass("tab-selected");
-        $("#userinfo").addClass("tab-selected");
+    $(".back2").click(function(){
+        $(".signup_step1").show();
+        $(".signup_step2").hide();
+        $(".companyinfo").removeClass("tab-selected");
+        $(".userinfo").addClass("tab-selected");
         $('html, body').animate({scrollTop: '0px'}, 300);
     });
 
-    $("#next2").click(function(){
-        $("#signup_step3").show();
-        $("#signup_step2").hide();
-        $("#companyinfo").removeClass("tab-selected");
-        $("#success").addClass("tab-selected");
-        $('html, body').animate({scrollTop: '0px'}, 300);
+    $(".next3").click(function(){
+        $(".signup_step3").hide();
+        $(".userinfo").removeClass("tab-selected");
+        window.location.href = "/login";
     });
 });
