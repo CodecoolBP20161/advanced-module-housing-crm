a06ed73f25cd9171d85035ac54d7ade751b663ff
balag3 <carbalage@gmail.com>
2017-03-13 13:49:24 +0100

    Adds post route for company status change, adds status based error message

--- src/main/java/com/codecool/hccrm/controller/admin/AdminController.java
+++ src/main/java/com/codecool/hccrm/controller/admin/AdminController.java
@@ -1,8 +1,14 @@
 package com.codecool.hccrm.controller.admin;
 
+import com.codecool.hccrm.model.Company;
+import com.codecool.hccrm.model.CompanyStatus;
+import com.codecool.hccrm.service.CompanyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,10 +22,22 @@ import static com.codecool.hccrm.logging.LogFormatter.FORMAT;
 public class AdminController {
     private Logger logger = LoggerFactory.getLogger(AdminController.class);
 
+    @Autowired
+    CompanyService companyService;
+
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
     public String adminIndex() {
         logger.info(FORMAT.getFormatter() + "Admin page is called.");
 
+        return "admin/admin_index";
+    }
+
+    @RequestMapping(value = {"admin/{company_id}/status"},method = RequestMethod.POST, consumes = "text/plain")
+    public String changeCompanyStatus(@PathVariable("company_id") String companyId, @RequestBody String status) {
+        Company company = companyService.findById(new Long(companyId));
+        CompanyStatus newStatus = CompanyStatus.valueOf(status);
+        company.setCompanyStatus(newStatus);
+        companyService.save(company);
         return "admin/admin_index";
     }
 }
--- src/main/resources/static/js/admin/table.js
+++ src/main/resources/static/js/admin/table.js
@@ -1,12 +1,13 @@
-$(document).ready(function() {
+ $(document).ready(function() {
     // Setup - add a text input to each footer cell
-    $('#company_list tfoot th').each(function () {
-        var title = $(this).text();
-        $(this).html('<input type="text" placeholder="Search ' + title + '" />');
-    });
+    // $('#company_list tfoot th').each(function () {
+    //     var title = $(this).text();
+    //     $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+    // });
 
     // Setup datatable
-    var table = $('#company_list').DataTable({
+    // var table =
+        $('#company_list').DataTable({
         ajax: {
             url: 'admin/company',
             dataSrc: '_embedded.company'
@@ -15,25 +16,37 @@ $(document).ready(function() {
             {data: 'companyName'},
             {data: 'taxNumber'},
             {data: 'premise'},
-            {data: 'createDate'}
-        ],
-        stateSave: true
+            {data: 'createDate'},
+            {
+                data: 'companyStatus',
+                "render": function ( data, type, full, meta ) {
+                return '<a href="/home">Download</a>';
+            }
+     }
+
+        ]
     });
+
+     // "createDate" : "2017-03-13",
+     //     "companyName" : "Landrum Temporary Services",
+     //     "taxNumber" : "1234567892",
+     //     "premise" : "Inverlaw",
+     //     "companyStatus" : "REJECTED",
 
     // TODO: LOCALSTORAGE NOT SAVING ADDITIONAL SEARCH QUERIES
     // BUT SAVING THE RESULT, FOR QUICK FIX -> delete the empty input fields
 
     // Apply the search
-    table.columns().every(function () {
-        var that = this;
-
-        $('input', this.footer()).on('keyup change', function () {
-            if (that.search() !== this.value) {
-                that
-                    .search(this.value)
-                    .draw();
-            }
-        });
-    });
+    // table.columns().every(function () {
+    //     var that = this;
+    //
+    //     $('input', this.footer()).on('keyup change', function () {
+    //         if (that.search() !== this.value) {
+    //             that
+    //                 .search(this.value)
+    //                 .draw();
+    //         }
+    //     });
+    // });
 
 });
